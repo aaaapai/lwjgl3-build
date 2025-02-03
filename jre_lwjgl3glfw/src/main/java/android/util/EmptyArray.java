4cacad78ec09eea313addefa5a836d4dab17832f
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-02-03 22:11:21 +0800

    Update EmptyArray.java

--- jre_lwjgl3glfw/src/main/java/android/util/EmptyArray.java
+++ jre_lwjgl3glfw/src/main/java/android/util/EmptyArray.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 The Android Open Source Project
+ * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,18 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package android.util;
 
+import org.jspecify.annotations.*;
+
+/**
+ * Empty array is immutable. Use a shared empty array to avoid allocation.
+ *
+ * @hide
+ */
 public final class EmptyArray {
     private EmptyArray() {}
-    public static final boolean[] BOOLEAN = new boolean[0];
-    public static final byte[] BYTE = new byte[0];
-    public static final char[] CHAR = new char[0];
-    public static final double[] DOUBLE = new double[0];
-    public static final int[] INT = new int[0];
-    public static final Class<?>[] CLASS = new Class[0];
-    public static final Object[] OBJECT = new Object[0];
-    public static final String[] STRING = new String[0];
-    public static final Throwable[] THROWABLE = new Throwable[0];
-    public static final StackTraceElement[] STACK_TRACE_ELEMENT = new StackTraceElement[0];
+
+    public static final @NonNull boolean[] BOOLEAN = new boolean[0];
+    public static final @NonNull byte[] BYTE = new byte[0];
+    public static final @NonNull char[] CHAR = new char[0];
+    public static final @NonNull double[] DOUBLE = new double[0];
+    public static final @NonNull float[] FLOAT = new float[0];
+    public static final @NonNull int[] INT = new int[0];
+    public static final @NonNull long[] LONG = new long[0];
+    public static final @NonNull Object[] OBJECT = new Object[0];
+    public static final @NonNull String[] STRING = new String[0];
 }
